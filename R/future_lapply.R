1940013ab94f506424985ce5c5164095e7ed9059
hb <hb@aroma-project.org>
2018-06-15 18:57:29 -0700

    CLEANUP: Move all RNG related functions to R/rng.R and added internal make_rng_seeds()

--- R/future_lapply.R
+++ R/future_lapply.R
@@ -94,7 +94,7 @@
 #'
 #' @importFrom globals globalsByName cleanup
 #' @importFrom future future resolve values as.FutureGlobals nbrOfWorkers getGlobalsAndPackages FutureError
-#' @importFrom parallel nextRNGStream nextRNGSubStream splitIndices
+#' @importFrom parallel splitIndices
 #' @importFrom utils capture.output head str
 #' @export
 future_lapply <- function(X, FUN, ..., future.globals = TRUE, future.packages = NULL, future.seed = FALSE, future.lazy = FALSE, future.scheduling = 1.0) {
@@ -248,87 +248,15 @@ future_lapply <- function(X, FUN, ..., future.globals = TRUE, future.packages = 
   ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   ## 3. Reproducible RNG (for sequential and parallel processing)
   ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  seed <- future.seed
+  seeds <- make_rng_seeds(nX, seed = future.seed, debug = debug)
 
-  ## Placeholder for all RNG stream seeds.
-  seeds <- NULL
-  
-  ## Don't use RNGs? (seed = FALSE)
-  if (is.logical(seed) && !is.na(seed) && !seed) seed <- NULL
-
-  # Use RNGs?
-  if (!is.null(seed)) {
-    if (debug) mdebug("Generating random seeds ...")
-
-    ## future_lapply() should return with the same RNG state regardless of
-    ## future strategy used. This is be done such that RNG kind is preserved
-    ## and the seed is "forwarded" one step from what it was when this
-    ## function was called. The forwarding is done by generating one random
-    ## number. Note that this approach is also independent on length(X) and
-    ## the diffent FUN() calls.
+  ## If RNG seeds are used (given or generated), make sure to reset
+  ## the RNG state afterward
+  if (!is.null(seeds)) {
     oseed <- next_random_seed()
-    on.exit(set_random_seed(oseed))    
-
-    ## A pregenerated sequence of random seeds?
-    if (is.list(seed)) {
-      if (debug) mdebug("Using a pre-define stream of random seeds ...", nX)
-      
-      nseed <- length(seed)
-      if (nseed != nX) {
-        stop("Argument 'seed' is a list, which specifies the sequence of seeds to be used for each element in 'X', but length(seed) != length(X): ", nseed, " != ", nX)
-      }
-
-      ## Assert same type of RNG seeds?
-      ns <- unique(unlist(lapply(seed, FUN = length), use.names = FALSE))
-      if (length(ns) != 1) {
-        stop("The elements of the list specified in argument 'seed' are not all of the same lengths (did you really pass RNG seeds?): ", hpaste(ns))
-      }
-
-      ## Did use specify scalar integers as meant for set.seed()?
-      if (ns == 1L) {
-        stop("Argument 'seed' is invalid. Pre-generated random seeds must be valid .Random.seed seeds, which means they should be all integers and consists of two or more elements, not just one.")
-      }
-
-      types <- unlist(lapply(seed, FUN = typeof), use.names = FALSE)
-      if (!all(types == "integer")) {
-        stop("The elements of the list specified in argument 'seed' are not all integers (did you really pass RNG seeds?): ", hpaste(unique(types)))
-      }
-      
-      ## Check if valid random seeds are specified.
-      ## For efficiency, only look at the first one.
-      if (!is_valid_random_seed(seed[[1]])) {
-        stop("The list in argument 'seed' does not seem to hold elements that are valid .Random.seed values: ", capture.output(str(seeds[[1]])))
-      }
-
-      seeds <- seed
-      
-      if (debug) mdebug("Using a pre-define stream of random seeds ... DONE", nX)
-    } else {
-      if (debug) mdebug("Generating random seed streams for %d elements ...", nX)
-      
-      ## Generate sequence of _all_ RNG seeds starting with an initial seed
-      ## '.seed' that is based on argument 'seed'.
-      .seed <- as_lecyer_cmrg_seed(seed)
-
-      seeds <- vector("list", length = nX)
-      for (ii in seq_len(nX)) {
-        ## RNG substream seed used in call FUN(X[[ii]], ...):
-        ## This way each future can in turn generate further seeds, also
-        ## recursively, with minimal risk of generating the same seeds as
-        ## another future. This should make it safe to recursively call
-        ## future_lapply(). /HB 2017-01-11
-        seeds[[ii]] <- nextRNGSubStream(.seed)
-        
-        ## Main random seed for next iteration (= ii + 1)
-        .seed <- nextRNGStream(.seed)
-      }
+    on.exit(set_random_seed(oseed))
+  }
   
-      if (debug) mdebug("Generating random seed streams for %d elements ... DONE", nX)
-    }
-    
-    if (debug) mdebug("Generating random seeds ... DONE")
-  } ## if (!is.null(seed))
-
   
   ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   ## 4. Load balancing ("chunking")

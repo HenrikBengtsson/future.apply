bec43cfaad45c0a787e9b9b71d7fb6d05f5e5f3a
Henrik Bengtsson <hb@aroma-project.org>
2018-08-26 11:05:34 -0700

    tweak [ci skip]

--- revdep/run.R
+++ revdep/run.R
@@ -41,7 +41,7 @@ check <- function() {
 
 todo <- function() {
   pkgs <- tryCatch(revdep_todo(), error = function(ex) NA)
-  if (is.na(pkgs)) {
+  if (length(pkgs) == 1L && is.na(pkgs)) {
     cat("Revdepcheck has not been initiated\n")
   } else if (length(pkgs) == 0) {
     cat("There are no packages on the revdepcheck todo list\n")

ebda4df0825f0d63123f7dba16e059c71a81808b
Henrik Bengtsson <hb@aroma-project.org>
2021-08-09 23:48:40 +0200

    TESTS: Add (another) tests for global '...' [#61]

--- tests/future_sapply.R
+++ tests/future_sapply.R
@@ -41,6 +41,16 @@ for (strategy in supportedStrategies()) {
       }
     }
   }
+
+  ## https://github.com/HenrikBengtsson/future.apply/issues/61
+  compute <- function(a, x_vec) a + x_vec
+  call_compute <- function(..., x_vec = 1:2){
+    compute_with_dots <- function(x) compute(..., x_vec = x)
+    future_sapply(x_vec, FUN = compute_with_dots)
+  }
+  y <- call_compute(0L)
+  print(y)
+  stopifnot(identical(y, 1:2))
   
   plan(sequential)
   message(sprintf("*** strategy = %s ... done", sQuote(strategy)))

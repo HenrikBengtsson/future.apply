983cc4fd83b9f344734f1f0424f903ee6cbd1e07
Henrik Bengtsson <hb@aroma-project.org>
2022-05-25 18:17:30 -0700

    TESTS: Truth should be produced using base-R functions

--- tests/stdout.R
+++ tests/stdout.R
@@ -2,17 +2,19 @@ source("incl/start.R")
 
 message("*** future_*apply() and 'future.stdout' ...")
 
-options(future.debug = FALSE)
+options(future.debug = TRUE)
 
 truth <- list()
+
 out <- utils::capture.output({
-  y <- future_lapply(1:0, FUN = function(x) {
+  y <- lapply(1:0, FUN = function(x) {
     print(x)
   })
 })
 truth[["lapply"]] <- list(value = y, stdout = out)
+
 out <- utils::capture.output({
-  y <- future_mapply(1:0, 0:1, FUN = function(x, y) {
+  y <- mapply(1:0, 0:1, FUN = function(x, y) {
     print(list(x = x, y = y))
   })
 })
@@ -26,19 +28,19 @@ for (cores in 1:availCores) {
     message(sprintf("* plan('%s') ...", strategy))
     plan(strategy)
 
-    for (fun in c("lapply", "mapply")) {
+    for (fun in names(truth)) {
       for (stdout in c(FALSE, TRUE, NA)) {
         message(sprintf("* future_%s(x, ..., future.stdout = %s) ...", fun, stdout))
   
         out <- utils::capture.output({
           if (fun == "lapply") {
             y <- future_lapply(1:0, FUN = function(x) {
-              Sys.sleep(x / 2)
+              Sys.sleep(x / 2)  ## make futures resolve out of order
 	      print(x)
             }, future.stdout = stdout)
 	  } else if (fun == "mapply") {
             y <- future_mapply(1:0, 0:1, FUN = function(x, y) {
-              Sys.sleep(x / 2)
+              Sys.sleep(x / 2)  ## make futures resolve out of order
               print(list(x = x, y = y))
             }, future.stdout = stdout)
 	  }

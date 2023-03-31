0bfa5714484afd0c40e45fc39747987ce215be31
Henrik Bengtsson <hb@aroma-project.org>
2023-03-30 22:39:31 -0700

    Temporarily add support for how chunking is subsetting via env var R_FUTURE_APPLY_CHUNKWITH [#109]

--- R/chunking.R
+++ R/chunking.R
@@ -1,4 +1,8 @@
 `chunkWith[[` <- function(x, subset) {
+  if (getOption("future.apply.chunkWith", "[") == "[") {
+    return(x[subset])
+  }
+  
   ## Make sure to preserve behavior of S3 method-specific
   ## `[[` subsetting like lapply() and mapply() does it
   ny <- length(subset)
